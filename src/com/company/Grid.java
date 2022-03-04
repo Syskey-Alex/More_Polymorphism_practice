6ef71ac669abc1296c1666a4256a1ad24ac5c637
16Ionita_A <16Ionita_A@bexleygs.internal>
2022-03-04 11:36:55 +0000

    Initial commit

---
+++ src/com/company/Grid.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Grid {
+private int myGrid[][] = new int[10][10];
+
+public void myGrid() {
+       for(int i = 0; i < myGrid.length; i++) {
+        for(int j = 0; j < myGrid.length; j++) {
+            System.out.println(myGrid[i][j]);
+        }
+
+    }
+}
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Grid myGrid = new Grid();
+    myGrid.myGrid();
+    }
+}
